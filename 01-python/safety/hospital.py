ea5a67fce56d131fb5303683d34668d9616b07a7
Yunshuang <ystao@bu.edu>
2020-09-18 17:19:17 -0400

    Not finished

--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -23,4 +23,57 @@ def draw_hospital():
     print('|           |           |           |')
     print('+-----------------------------------+')
 
-draw_hospital()
+def show_hospital(width):
+    top(width)
+    symbol1(width)
+    symbol2(width)
+    symbol3(width)
+    symbol3(width)
+    symbol2(width)
+    symbol1(width)
+
+def top(width):
+    for i in range(width*10+2):
+        print(' ', end='')
+    print('+', end='')
+    for i in range(width*10+1):
+        print('-', end='')
+    print('+',end='')
+    for i in range(width*10+2):
+        print(' ',end='')
+    print('')
+
+def symbol1(width):
+    for i in range(width*10+2):
+        print(' ', end='')
+    print('|',end='')
+    for i in range(width*5-1):
+        print(' ', end='')
+    print('+-+', end='')
+    for i in range(width*5-1):
+        print(' ', end='')
+    print('|')
+
+def symbol2(width):
+    for i in range(width*10+2):
+        print(' ', end='')
+    print('|',end='')
+    for i in range(width*5-1):
+        print(' ', end='')
+    print('| |', end='')
+    for i in range(width*5-1):
+        print(' ', end='')
+    print('|')
+
+def symbol3(width):
+    for i in range(width*10+2):
+        print(' ', end='')
+    print('|',end='')
+    for i in range(width*5-4):
+        print(' ', end='')
+    print('+--   --+', end='')
+    for i in range(width*5-4):
+        print(' ', end='')
+    print('|')
+
+show_hospital(4)
