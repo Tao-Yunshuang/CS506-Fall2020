23273fcd3a6fec8e92b96a1e203310ec5737d11a
Yunshuang <ystao@bu.edu>
2020-09-19 16:13:43 -0400

    modified

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,17 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    data = []
+    with open(csv_file_path) as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for line in csv_reader:
+            data.append(line)
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j].isdigit():
+                data[i][j] = int(data[i][j])
+    return data
